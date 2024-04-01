35fd7ecbc708e9834771d1a1262acac0cdc9493a
Ungs <iqeq1945@gmail.com>
2024-04-01 21:10:31 +0900

    feat: socket.io 기능, database 기본 구축

---
+++ src/socket/socket.gateway.ts
@@ -0,0 +1,62 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+interface Data {
+  roomId: string | string[];
+  message: string;
+}
+
+@WebSocketGateway({ namespace: 'wakttu' })
+export class SocketGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  public server: Server;
+
+  public clients: {
+    [socketId: string]: string;
+  } = {};
+  handleConnection(client: Socket): void {
+    console.log(`connect: ${client.id}`);
+    this.clients[client.id] = client.id;
+    console.log(this.clients);
+  }
+
+  afterInit() {
+    console.log('socket is open!');
+  }
+
+  handleDisconnect(client: Socket): void {
+    console.log(`disconnect: ${client.id}`);
+    delete this.clients[client.id];
+  }
+
+  @SubscribeMessage('chat')
+  handleMessage(@MessageBody() { roomId, message }: Data): void {
+    this.server.to(roomId).emit('chat', message);
+  }
+
+  @SubscribeMessage('enter')
+  handleEnter(
+    @MessageBody() roomId: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    client.join(roomId);
+    this.server.to(roomId).emit('enter', `${client.id}이 입장`);
+  }
+
+  @SubscribeMessage('exit')
+  handleExit(@MessageBody() roomId: string, @ConnectedSocket() client: Socket) {
+    client.leave(roomId);
+    this.server.to(roomId).emit('exit', `${client.id}이 퇴장`);
+  }
+}
