ea399e959d65fafff60aaf9ee602a86a820c9e0e
Ungs <iqeq1945@gmail.com>
2024-04-10 21:00:52 +0900

    feat : passport local

--- src/serializer.ts
+++ src/serializer.ts
@@ -21,8 +21,11 @@ export class SessionSerializer extends PassportSerializer {
     payload: any,
     done: (err: any, user?: any) => void,
   ): Promise<any> {
-    const user = await this.userService.findById(payload.id);
+    const response = await this.userService.findById(payload.id);
     //    console.log(user, 'deserializeUser'); // 테스트 시 확인
+    const user = JSON.parse(JSON.stringify(response));
+    delete user.password;
+    console.log(user);
     return user ? done(null, user) : done(null, null);
   }
 }
