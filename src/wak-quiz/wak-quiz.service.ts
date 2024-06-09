47ccc90114eb92dfb715386244c14c7e6dab5bad
Ungs <iqeq1945@gmail.com>
2024-06-09 19:23:13 +0900

    feat : quiz 기능 추가

--- src/wak-quiz/wak-quiz.service.ts
+++ src/wak-quiz/wak-quiz.service.ts
@@ -1,13 +1,51 @@
 import { Inject, Injectable, forwardRef } from '@nestjs/common';
-import { SocketGateway } from 'src/socket/socket.gateway';
+import { Room } from 'src/room/entities/room.entity';
+import { Game, SocketGateway } from 'src/socket/socket.gateway';
 import { SocketService } from 'src/socket/socket.service';
+
+class Answer {
+  index: number;
+  submit: string;
+}
+
+class Rule {
+  answer: Answer[];
+}
 
 @Injectable()
 export class WakQuizService {
+  public rules: {
+    [roomId: string]: Rule;
+  } = {};
   constructor(
     @Inject(forwardRef(() => SocketGateway))
     private readonly socketGateway: SocketGateway,
     private readonly socketService: SocketService,
   ) {}
   public server;
+
+  async handleStart(roomId: string, roomInfo: Room, game: Game) {
+    game.total = game.users.length;
+    game.quiz = await this.socketService.getQuizList(roomInfo.round);
+    roomInfo.start = (
+      await this.socketService.setStart(roomId, roomInfo.start)
+    ).start;
+    this.rules[roomId] = new Rule();
+    this.server.to(roomId).emit('wak-quiz.start', game);
+  }
+  handleRound(roomId: string, roomInfo: Room, game: Game) {
+    const curRound = game.round++;
+    const lastRound = roomInfo.round;
+    if (curRound === lastRound) {
+      this.server.to(roomId).emit('end', { message: 'end' });
+      return;
+    }
+    game.target = game.quiz[curRound];
+    this.rules[roomId].answer.splice(0);
+    this.server.to(roomId).emit('wak-quiz.round', game);
+  }
+
+  handleAnswer(roomId: string, index: number, submit: string) {
+    this.rules[roomId].answer.push({ index, submit });
+  }
 }
