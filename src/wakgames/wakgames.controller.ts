70ace0cb6bb25ba70f12ea3113317dad0e3ed7eb
Ungs <iqeq1945@gmail.com>
2024-06-30 20:52:23 +0900

    fix : code

--- src/wakgames/wakgames.controller.ts
+++ src/wakgames/wakgames.controller.ts
@@ -6,13 +6,9 @@ import {
   Put,
   Query,
   Session,
-  UnauthorizedException,
-  UseGuards,
 } from '@nestjs/common';
 import { WakgamesService } from './wakgames.service';
-import { WakgamesGuard } from './wakgames.guard';
 
-@UseGuards(WakgamesGuard)
 @Controller('wakta')
 export class WakgamesController {
   constructor(private readonly wakgamesService: WakgamesService) {}
@@ -23,15 +19,14 @@ export class WakgamesController {
       session.accessToken,
     );
     if (response.status === 401) {
-      const { data, response } = await this.wakgamesService.updateToken(
+      let { data, response } = await this.wakgamesService.updateToken(
         session.refreshToken,
       );
-      if (response.status !== 200) throw new UnauthorizedException();
+      console.log(data, response);
       session.accessToken = data.accessToken;
       session.refreshToken = data.refreshToken;
-      return await this.getProfile(session);
     }
-    return data;
+    return { data, response };
   }
 
   @Get('achieve')
@@ -39,62 +34,31 @@ export class WakgamesController {
     const { data, response } = await this.wakgamesService.getAchieve(
       session.accessToken,
     );
-    if (response.status === 401) {
-      const { data, response } = await this.wakgamesService.updateToken(
-        session.refreshToken,
-      );
-      if (response.status !== 200) throw new UnauthorizedException();
-      session.accessToken = data.accessToken;
-      session.refreshToken = data.refreshToken;
-      return await this.getAchieve(session);
-    }
-    return data;
+    console.log(data, response);
+    return { data, response };
   }
 
-  @Post('achieve')
+  @Post('acheive')
   async postAchieve(@Query() query, @Session() session: Record<string, any>) {
     const { data, response } = await this.wakgamesService.postAchieve(
       query,
       session.accessToken,
     );
-    if (response.status === 401) {
-      const { data, response } = await this.wakgamesService.updateToken(
-        session.refreshToken,
-      );
-      if (response.status !== 200) throw new UnauthorizedException();
-      session.accessToken = data.accessToken;
-      session.refreshToken = data.refreshToken;
-      return await this.postAchieve(query, session);
-    } else if (response.status === 409) {
-      return {
-        status: response.status,
-        message: '이미 달성된 도전과제입니다!',
-      };
-    } else if (response.status === 404) {
-      return {
-        status: response.status,
-        message: '해당 도전 과제를 찾을 수 없습니다.',
-      };
-    }
-    return data;
+    console.log(data, response);
+    return { data, response };
   }
 
   @Get('stat')
-  async getStat(@Query() query, @Session() session: Record<string, any>) {
+  async getStat(
+    @Query('id') id: string,
+    @Session() session: Record<string, any>,
+  ) {
     const { data, response } = await this.wakgamesService.getStat(
-      query,
+      id,
       session.accessToken,
     );
-    if (response.status === 401) {
-      const { data, response } = await this.wakgamesService.updateToken(
-        session.refreshToken,
-      );
-      if (response.status !== 200) throw new UnauthorizedException();
-      session.accessToken = data.accessToken;
-      session.refreshToken = data.refreshToken;
-      return await this.getStat(query, session);
-    }
-    return data;
+    console.log(data, response);
+    return { data, response };
   }
 
   @Put('stat')
@@ -103,20 +67,7 @@ export class WakgamesController {
       body,
       session.accessToken,
     );
-    if (response.status === 401) {
-      const { data, response } = await this.wakgamesService.updateToken(
-        session.refreshToken,
-      );
-      if (response.status !== 200) throw new UnauthorizedException();
-      session.accessToken = data.accessToken;
-      session.refreshToken = data.refreshToken;
-      return await this.putStat(body, session);
-    }
-    return data;
-  }
-
-  @Put('result')
-  async putResult(@Body() body, @Session() session: Record<string, any>) {
-    return await this.wakgamesService.putResult(body, session);
+    console.log(data, response);
+    return { data, response };
   }
 }
