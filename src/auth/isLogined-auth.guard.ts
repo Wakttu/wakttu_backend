73bb94d227804046fca2566176707787121126f2
iqeq1945 <iqeq1945@gmail.com>
2024-07-04 15:38:59 +0900

    feat : user guard 로직 추가 및 schema수정

--- src/auth/isLogined-auth.guard.ts
+++ src/auth/isLogined-auth.guard.ts
@@ -6,11 +6,12 @@ import {
 } from '@nestjs/common';
 
 @Injectable()
-export class IsNotLoginedGuard implements CanActivate {
+export class IsLoginedGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const check = this.checkUser(request.session);
-    if (!check) throw new ForbiddenException({ message: '로그인 중 입니다!' });
+    if (check)
+      throw new ForbiddenException({ message: '로그인 중이 아닙니다!' });
     return true;
   }
   checkUser(session) {
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,14 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({
+    example: ['https://placehold.co/600x400', 'https://placehold.co/1200x600'],
+    description: 'emoji url 주소 배열',
+  })
+  @IsOptional()
+  @IsString({ each: true })
+  keyboard?: string[];
+}
