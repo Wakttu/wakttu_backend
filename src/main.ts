432263f7d8cc725a8d6bf19b67ed9a491186da95
Ungs <iqeq1945@gmail.com>
2024-07-22 21:48:29 +0900

    fix : domain issue

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import * as MongoDBStore from 'connect-mongodb-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'https://localhost:3000', credentials: true });
+  app.enableCors({ origin: true, credentials: true });
   const MongoStore = MongoDBStore(session);
 
   const store = new MongoStore({
@@ -26,6 +26,7 @@ async function bootstrap() {
       httpOnly: true,
       sameSite: 'none',
       secure: true,
+      domain: 'localhost',
     },
     store: store,
   });
