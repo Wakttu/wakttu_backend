1ea42473666cb2eea8646f900edbc04a2e413058
iqeq1945 <iqeq1945@gmail.com>
2024-10-15 16:14:26 +0900

    feat : achieve

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prisma: PrismaService) {}
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getAchieve() {
+    return await this.prisma.achieve.findMany({});
   }
 }
