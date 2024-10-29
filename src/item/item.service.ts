919d7e74c2d50a2ae34a36004310fa30f68661d2
iqeq1945 <iqeq1945@gmail.com>
2024-10-29 11:13:55 +0900

    feat : optimization

--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -6,8 +6,12 @@ export class ItemService {
   constructor(private readonly prisma: PrismaService) {}
 
   async findAll() {
-    return await this.prisma.item.findMany({
-      orderBy: { id: 'asc' },
-    });
+    try {
+      return await this.prisma.item.findMany({
+        orderBy: { id: 'asc' },
+      });
+    } catch (error) {
+      throw new Error(`아이템 조회 중 오류가 발생했습니다: ${error.message}`);
+    }
   }
 }
